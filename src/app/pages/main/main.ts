230309b30efaeec2c14f5e9989cdf41d3716ad0b
Umeyrp <umeyr.kemiksiz@gmail.com>
2026-08-05 16:53:20 +0200

    feat: implement footer component; enhance references section with testimonials; update skills section to reflect new logo paths

--- src/app/pages/main/main.ts
+++ src/app/pages/main/main.ts
@@ -7,10 +7,11 @@ import { Skills } from './skills/skills';
 import { ArrowLeft } from './arrow-left/arrow-left';
 import { PortfolioOverview } from './portfolio-overview/portfolio-overview';
 import { References } from './references/references';
+import { Footer } from './footer/footer';
 
 @Component({
     selector: 'app-main',
-    imports: [Header, Hero, AboutMe, ArrowRight, Skills, ArrowLeft, PortfolioOverview, References],
+    imports: [Header, Hero, AboutMe, ArrowRight, Skills, ArrowLeft, PortfolioOverview, References, Footer],
     templateUrl: './main.html',
     styleUrl: './main.scss',
 })
