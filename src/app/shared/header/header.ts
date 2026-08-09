1e2d17a929716653878908af3418a5bd0da76697
Umeyrp <umeyr.kemiksiz@gmail.com>
2026-08-09 03:10:20 +0200

    feat: enhance header component with burger menu and scroll functionality; add scroll margin to sections

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,9 +1,10 @@
 import { Component, inject, input, signal } from '@angular/core';
 import { LanguageService } from '../../services/language';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-header',
-    imports: [],
+    imports: [TranslatePipe],
     templateUrl: './header.html',
     styleUrl: './header.scss',
 })
@@ -11,7 +12,23 @@ export class Header {
     showLanguageButtons = input(true);
     protected languageService = inject(LanguageService);
 
+    isMenuOpen = signal(false);
+
     switchLanguage(language: string) {
         this.languageService.switchLanguage(language);
+    }
+
+    toggleMenu() {
+        this.isMenuOpen.update((open) => !open);
+    }
+
+    scrollToSection(id: string) {
+        this.isMenuOpen.set(false);
+        setTimeout(() => {
+            document.getElementById(id)?.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start',
+            });
+        }, 50);
     }
 }
